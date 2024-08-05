97448d6c182aa365c0f4a557cafcfbe3c14290f5
Yxiox <bruno.dela.goncalves@gmail.com>
2024-08-05 19:00:45 -0300

    Reapply "base-files"
    This reverts commit 416fb9a81d7cceabcc33806142bdcebda6b33144.

---
+++ Pages/LoadHeader.js
@@ -0,0 +1,25 @@
+document.write(`
+    <header>
+      <div
+        style="
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          gap: 15px;
+        "
+      >
+        <img src="../../Images/Icon.png" height="70px" />
+        <h2>Frutificar</h2>
+      </div>
+      <div
+        style="
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          gap: 15px;
+        "
+      >
+        
+      </div>
+    </header>
+`);
