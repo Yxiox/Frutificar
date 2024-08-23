27a2681a777677a77ee1e1455cea40cbcc349a61
Yxiox <bruno.dela.goncalves@gmail.com>
2024-08-22 21:37:21 -0300

    Listagem
    Adicionado listagem no dropdown e adicionado

--- Pages/Venda/venda-script.js
+++ Pages/Venda/venda-script.js
@@ -1,30 +1,54 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     produtoInput = document.getElementById("produto");
     quantidadeInput = document.getElementById("qtd");
- });
+    form = document.getElementById("frutas");
+    localStorage.setItem("produtos", JSON.stringify([{nome:"Pera", preco:15}]));
+    
+    loadOptions();
+});
 
-lista=[{}];
+let lista=[{}];
 
-var listaProdutos=[{}];
+let listaProdutos=[{}];
+let listaNomes=["Pera", "Abacate"];
 
-function getProdutoInfo(produto){
-    listaProdutos=JSON.parse(localStorage.getItem("produtos"));
-    listaProdutos.forEach(pd => {
-        if(pd.nome = produto){
-            return pd;
+let preco = 0;
+
+function getProdutoByName(produto){
+    getProdutoInfo
+    for(let i = 0; i < listaProdutos.length; i++){
+        if(listaProdutos[i].nome == produto){
+            return listaProdutos[i];
         }        
-    });
+    };
+}
+
+function getProdutoInfo(){
+    listaProdutos=JSON.parse(localStorage.getItem("produtos"));
+}
+
+function setOptions(){
+    getProdutoInfo();
+    console.log(listaNomes);
+    for(let i = 0; i < listaProdutos.length; i++){
+        listaNomes.push(listaProdutos[i].nome);
+    };
+}
+
+function loadOptions(){
+    setOptions();
+    for(let i = 0; i < listaNomes.length; i++){
+        form.innerHTML += "<option value=" + listaNomes[i] + "></option>"
+    };
+    
 }
 
 function adicionarLista(){
-    
     if(produtoInput.value != null && quantidadeInput.value != null){
         lista.push({
-            produto: getProdutoInfo(),
-            quantidade: quantidadeInput
+            produto: getProdutoByName(produtoInput.value),
+            quantidade: quantidadeInput.value
         });
+        console.log(lista);
     }
-
-
-
 }
