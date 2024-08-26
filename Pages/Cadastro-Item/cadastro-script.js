aadb89f6e5c3f8076a5c5500b4630b62b2a9c282
Yxiox <bruno.dela.goncalves@gmail.com>
2024-08-26 20:28:39 -0300

    fix 404

--- Pages/Cadastro-Item/cadastro-script.js
+++ Pages/Cadastro-Item/cadastro-script.js
@@ -1,53 +1,58 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-    nomeInput = document.getElementById("product-name");
-    descricaoInput = document.getElementById("product-description");
-    precoInput = document.getElementById("product-price");
-    listaProdutos = JSON.parse(localStorage.getItem("produtos"));
+  nomeInput = document.getElementById("product-name");
+  descricaoInput = document.getElementById("product-description");
+  precoInput = document.getElementById("product-price");
+  listaProdutos = JSON.parse(localStorage.getItem("produtos"));
 });
 
-let listaProdutos=[];
+let listaProdutos = [];
 
-function addContent(){
-    if (nomeInput.value != null){
-        if(precoInput != null){
-            if(descricaoInput.value != null){
-                if(listaProdutos != null){
-                    listaProdutos.push({
-                        nome:nomeInput.value,
-                        preco:parseFloat(precoInput.value),
-                        descricao:descricaoInput.value
-                    });
-                }
-                else{
-                    listaProdutos =[{
-                        nome:nomeInput.value,
-                        preco:parseFloat(precoInput.value),
-                        descricao:descricaoInput.value
-                    }]; 
-                }                
-                localStorage.setItem("produtos",JSON.stringify(listaProdutos));
-                alert("Cadastrado com sucesso!");
-                window.location.reload();
-            }
-            else{
-                if(listaProdutos != null){
-                    listaProdutos.push({
-                        nome:nomeInput.value,
-                        preco:parseFloat(precoInput.value),
-                        descricao:descricaoInput.value
-                    });
-                }
-                else{
-                    listaProdutos =[{
-                        nome:nomeInput.value,
-                        preco:parseFloat(precoInput.value),
-                        descricao:""
-                    }]; 
-                }
-                localStorage.setItem("produtos", JSON.stringify(listaProdutos));
-                alert("Cadastrado com sucesso!");
-                window.location.reload();
-            }
+function addContent() {
+  let nomeInput = document.getElementById("product-name");
+  let descricaoInput = document.getElementById("product-description");
+  let precoInput = document.getElementById("product-price");
+
+  if (nomeInput.value != null) {
+    if (precoInput != null) {
+      if (descricaoInput.value != null) {
+        if (listaProdutos != null) {
+          listaProdutos.push({
+            nome: nomeInput.value,
+            preco: parseFloat(precoInput.value),
+            descricao: descricaoInput.value,
+          });
+        } else {
+          listaProdutos = [
+            {
+              nome: nomeInput.value,
+              preco: parseFloat(precoInput.value),
+              descricao: descricaoInput.value,
+            },
+          ];
         }
+        localStorage.setItem("produtos", JSON.stringify(listaProdutos));
+        alert("Cadastrado com sucesso!");
+        window.location.reload();
+      } else {
+        if (listaProdutos != null) {
+          listaProdutos.push({
+            nome: nomeInput.value,
+            preco: parseFloat(precoInput.value),
+            descricao: descricaoInput.value,
+          });
+        } else {
+          listaProdutos = [
+            {
+              nome: nomeInput.value,
+              preco: parseFloat(precoInput.value),
+              descricao: "",
+            },
+          ];
+        }
+        localStorage.setItem("produtos", JSON.stringify(listaProdutos));
+        alert("Cadastrado com sucesso!");
+        window.location.reload();
+      }
     }
+  }
 }
